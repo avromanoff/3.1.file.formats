fc3849ff75086349e31eb86f1a17e2fccb0b4897
Alexey Romanoff <romanoff.alexey@gmail.com>
2020-03-25 20:41:09 +0300

    update

--- formats_json.py
+++ formats_json.py
@@ -4,7 +4,7 @@ import json
 
 def top_words_list():
     # ищет топ-10 из счетчика
-    long_words = []
+    long_words = {}
     word_top = ''
     for x in range(10):
         word_counter = 0
@@ -13,7 +13,7 @@ def top_words_list():
                 if cnt.get(words) > word_counter:
                     word_top = words
                     word_counter = cnt.get(words)
-        long_words.append(word_top)
+        long_words[word_top] = word_counter
     return long_words
 
 
@@ -26,12 +26,11 @@ descriptions = []
 for news in item_json:
     descriptions += news.get('description').split(' ')
 
-
-# description_sort = sorted(descriptions)
+descriptions_lower = [item.lower() for item in descriptions]
 
 
 cnt = Counter()
-for word in descriptions:
+for word in descriptions_lower:
     if len(word) > 5:
         cnt[word] += 1
 
